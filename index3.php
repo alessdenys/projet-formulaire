ca91b72b2c25d6dc9d531bb709767b888dcf0602
Belinda Schepers <belindaschepers1@gmail.com>
2019-12-02 16:26:41 +0100

    3accessories

---
+++ index3.php
@@ -0,0 +1,264 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Hackers Poulette Pi Accessories</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/shop-homepage.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">All You Need, On One Site</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <div class="row">
+
+      <div class="col-lg-3">
+
+        <img src="Images/hackers-poulette-logo.png" alt="logo" class="logo">
+        <div class="list-group">
+          <a href="#" class="list-group-item">New Items</a>
+          <a href="#" class="list-group-item">Raspberry Pi Boards</a>
+          <a href="#" class="list-group-item">Raspberry Pi Accessories</a>
+          <a href="#" class="list-group-item">Industrial Compute Module</a>
+        </div>
+
+      </div>
+      <!-- /.col-lg-3 -->
+
+      <div class="col-lg-9">
+
+        <div id="carouselExampleIndicators" class="carousel slide my-4" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner" role="listbox">
+            <div class="carousel-item active">
+              <img class="d-block img-fluid" src="Images/pi-4-model-B.png" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="Images/compute-module3.jpg" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block img-fluid" src="Images/pi-3-model-A+.jpg" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi-keyboard.jpg" alt="Raspberry Pi Keyboard"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi Keyboard And Hub</a>
+                </h4>
+                <h5>17,97€</h5>
+                <p class="card-text">The official Raspberry Pi keyboard includes three USB ports for connecting external devices, available in raspberry red and white, or black and grey</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi-mouse.jpg" alt="Raspberry Pi Mouse"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi Mouse</a>
+                </h4>
+                <h5>10,99€</h5>
+                <p class="card-text">The official Raspberry Pi mouse, available in raspberry red and white, or black and grey</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi-tv-hat.jpg" alt="Raspberry Pi Tv Hat"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi Tv Hat</a>
+                </h4>
+                <h5>29,99€</h5>
+                <p class="card-text">This add-on board lets you receive digital DVB-T2 TV streams on your Raspberry Pi to view them or stream them over a network to other devices</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi3-modelB.jpg" alt="Raspberry Pi 3 model B"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi 3 Model B</a>
+                </h4>
+                <h5>44.99€</h5>
+                <p class="card-text">Single-board computer with wireless LAN and Bluetooth connectivity</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi2-model-B.jpg" alt="Raspberry Pi 2 model B"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi2 Model B</a>
+                </h4>
+                <h5>34.99€</h5>
+                <p class="card-text">The Raspberry Pi 2 Model B is the second-generation Raspberry Pi. It replaced the original Raspberry Pi 1 Model B+ in February 2015.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi1-modelB+.jpg" alt="Raspberry Pi1 model B+"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi1 model B+</a>
+                </h4>
+                <h5>35,99€</h5>
+                <p class="card-text">The Model B+ is the final revision of the original Raspberry Pi. It replaced the Model B in July 2014 and was superseded by the Raspberry Pi 2 Model B</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi1-model-A+.jpg" alt="Raspberry Pi1 model A+"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi1 model A+</a>
+                </h4>
+                <h5>39,99€</h5>
+                <p class="card-text">The Model A+ is the low-cost variant of the Raspberry Pi. It replaced the original Model A in November 2014.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi-zero-w.jpg" alt="Raspberry Pi Zero W"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi Zero W</a>
+                </h4>
+                <h5>29,99€</h5>
+                <p class="card-text">The Raspberry Pi Zero W extends the Pi Zero family and comes with added wireless LAN and Bluetooth connectivity.</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4 col-md-6 mb-4">
+            <div class="card h-100">
+              <a href="#"><img class="card-img-top" src="Images/pi-zero.jpg" alt="Raspberry Pi Zero"></a>
+              <div class="card-body">
+                <h4 class="card-title">
+                  <a href="#">Raspberry Pi Zero</a>
+                </h4>
+                <h5>9,99€</h5>
+                <p class="card-text">The Raspberry Pi Zero is half the size of a Model A+, with twice the utility. A tiny Raspberry Pi that’s affordable enough for any project!</p>
+              </div>
+              <div class="card-footer">
+                <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <!-- /.row -->
+
+      </div>
+      <!-- /.col-lg-9 -->
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>

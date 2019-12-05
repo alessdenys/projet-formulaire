06293a65be84b62a4043cdc0cd9d52cc6d1c47a6
Belinda Schepers <belindaschepers1@gmail.com>
2019-12-05 09:11:33 +0100

    commit to pull

--- contact.php
+++ contact.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -105,6 +106,8 @@
   <!-- Bootstrap core JavaScript -->
   <script src="vendor/jquery/jquery.min.js"></script>
   <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </form>
 
 </body>
 
